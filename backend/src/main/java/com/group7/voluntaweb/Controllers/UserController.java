350a57857b525c289f25e58d12f7e491309601b3
theroxd4n <d.serranoc.2017@alumnos.urjc.es>
2020-02-18 19:46:32 +0100

    WIP dynamic header

--- backend/src/main/java/com/group7/voluntaweb/Controllers/UserController.java
+++ backend/src/main/java/com/group7/voluntaweb/Controllers/UserController.java
@@ -1,99 +1,86 @@
 package com.group7.voluntaweb.Controllers;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.group7.voluntaweb.Components.UserComponent;
 import com.group7.voluntaweb.Models.User;
 import com.group7.voluntaweb.Repositories.UserRepository;
-
 import com.group7.voluntaweb.Services.UserService;
-
 
 @Controller
 public class UserController {
-	
-    @Autowired
-    private UserRepository userRepo;
-    
-    @Autowired
-    private UserService userService;
 
-	// inject via application.properties
-	@Value("${app.register.title}")
-	private String TITLE = "";
+	@Autowired
+	private UserRepository userRepo;
 
-	@Value("${app.registerUser.title}")
-	private String UTITLE = "";
-	
-	@Value("${app.registerONG.title}")
-	private String OTITLE = "";
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserComponent userComponent;
 
 	@GetMapping("/register")
 	public String register(Map<String, Object> model) {
-		model.put("title", TITLE);
+		model.put("title", "Regístrarse");
 
 		return "register";
 	}
-	
-	
+
 	@GetMapping("/register-user")
 	public String registerUser(Map<String, Object> model) {
-		model.put("title", UTITLE);
+		model.put("title", "Registrar usuario");
 
 		return "registerVolunteers";
 	}
-	
+
 	@PostMapping("/add-user")
-	public String addUser(@RequestParam String name, @RequestParam String surname, @RequestParam String email, @RequestParam String city, @RequestParam String telephone, @RequestParam String password, Map<String, Object> model) {
-		model.put("title", UTITLE);
-		
-		
+	public String addUser(@RequestParam String name, @RequestParam String surname, @RequestParam String email,
+			@RequestParam String city, @RequestParam String telephone, @RequestParam String password,
+			Map<String, Object> model) {
+		model.put("title", "Registrar usuario");
+
 		String enc_password = new BCryptPasswordEncoder().encode(password);
-		
+
 		List<String> roles = new ArrayList<String>();
-		
+
 		roles.add("ROLE_USER");
-		
-		
+
 		User user = new User(name, surname, email, enc_password, city, telephone, null, roles);
-		
-		
-		
+
 		this.userService.save(user);
-		
-		
-		
+
 		return "redirect:register";
 
 	}
-	
+
 	@GetMapping("/users")
 	public Iterable<User> listUsers() {
-	
+
 		return userRepo.findAll();
 	}
-	
+
 	@GetMapping("/login")
 	public String login(Map<String, Object> model, HttpSession sesion) {
 		model.put("title", "Iniciar sesión");
-		
+
+		Boolean logged = userComponent.isLoggedUser();
+		User user = userComponent.getLoggedUser();
+
+		model.put("logged", logged);
+		model.put("user", user);
+
 		return "login";
 	}
-	
-		
-
 
 }
--- backend/src/main/java/com/group7/voluntaweb/SecurityConfiguration.java
+++ backend/src/main/java/com/group7/voluntaweb/SecurityConfiguration.java
@@ -5,66 +5,63 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.group7.voluntaweb.Repositories.UserRepositoryAuthProvider;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
-    @Autowired
-    public UserRepositoryAuthProvider userRepoAuthProvider;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        
-    	// Public pages
-        http.authorizeRequests().antMatchers("/").permitAll();
-        	//Login pages
-        http.authorizeRequests().antMatchers("/login").permitAll();
-        http.authorizeRequests().antMatchers("/loginerror").permitAll();
-        	//Logout page
-        http.authorizeRequests().antMatchers("/logout").permitAll();
-        	//Register pages and save actions
-        http.authorizeRequests().antMatchers("/register").anonymous();
-        http.authorizeRequests().antMatchers("/register-ong").anonymous();
-        http.authorizeRequests().antMatchers("/add-ong").anonymous();
-        http.authorizeRequests().antMatchers("/register-user").anonymous();
-        http.authorizeRequests().antMatchers("/add-user").anonymous();
-        	//Assets
-        http.authorizeRequests().antMatchers("/css/**").permitAll();
-        http.authorizeRequests().antMatchers("/images/**").permitAll();
-        http.authorizeRequests().antMatchers("/js/**").permitAll();
-        http.authorizeRequests().antMatchers("/plugins/**").permitAll();
+	@Autowired
+	public UserRepositoryAuthProvider userRepoAuthProvider;
 
-        // Private pages (all other pages)
-        http.authorizeRequests().antMatchers("/admin/**").hasAnyRole("ROLE_ADMIN");;
-        http.authorizeRequests().anyRequest().authenticated();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 
-        // Login form
-        http.formLogin().loginPage("/login");
-        http.formLogin().usernameParameter("email");
-        http.formLogin().passwordParameter("password");
-        http.formLogin().defaultSuccessUrl("/");
-        http.formLogin().failureUrl("/loginerror");
+		// Public pages
+		http.authorizeRequests().antMatchers("/").permitAll();
+		// Login pages
+		http.authorizeRequests().antMatchers("/login").permitAll();
+		http.authorizeRequests().antMatchers("/loginerror").permitAll();
+		// Logout page
+		http.authorizeRequests().antMatchers("/logout").permitAll();
+		// Register pages and save actions
+		http.authorizeRequests().antMatchers("/register").anonymous();
+		http.authorizeRequests().antMatchers("/register-ong").anonymous();
+		http.authorizeRequests().antMatchers("/add-ong").anonymous();
+		http.authorizeRequests().antMatchers("/register-user").anonymous();
+		http.authorizeRequests().antMatchers("/add-user").anonymous();
+		// Assets
+		http.authorizeRequests().antMatchers("/css/**").permitAll();
+		http.authorizeRequests().antMatchers("/images/**").permitAll();
+		http.authorizeRequests().antMatchers("/js/**").permitAll();
+		http.authorizeRequests().antMatchers("/plugins/**").permitAll();
 
-        // Logout
-        http.logout().logoutUrl("/logout");
-        http.logout().logoutSuccessUrl("/");
-        
-        // Disable CSRF at the moment
-        http.csrf().disable();
-    }
+		// Private pages (all other pages)
+		http.authorizeRequests().antMatchers("/admin/**").hasAnyRole("ROLE_ADMIN");
+		;
+		http.authorizeRequests().anyRequest().authenticated();
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	
-        
-     // Database authentication provider
-        auth.authenticationProvider(userRepoAuthProvider);
+		// Login form
+		http.formLogin().loginPage("/login");
+		http.formLogin().usernameParameter("email");
+		http.formLogin().passwordParameter("password");
+		http.formLogin().defaultSuccessUrl("/login");
+		http.formLogin().failureUrl("/loginerror");
 
+		// Logout
+		http.logout().logoutUrl("/logout");
+		http.logout().logoutSuccessUrl("/");
 
-    }
+		// Disable CSRF at the moment
+		http.csrf().disable();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// Database authentication provider
+		auth.authenticationProvider(userRepoAuthProvider);
+
+	}
 
 }
