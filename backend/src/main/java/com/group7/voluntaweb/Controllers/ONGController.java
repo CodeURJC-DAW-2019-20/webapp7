315f8b54a8d3689109d523babac7d2717f938b91
sbsam <sbsam@users.noreply.github.com>
2020-02-23 23:13:44 +0100

    Added imports

--- backend/src/main/java/com/group7/voluntaweb/Controllers/ONGController.java
+++ backend/src/main/java/com/group7/voluntaweb/Controllers/ONGController.java
@@ -6,7 +6,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,26 +55,17 @@ public class ONGController {
 	}
 	
 	@PostMapping("/add-ong") // ONG REGISTER ACTION
-	public String addOng(@RequestParam String name, @RequestParam String email, @RequestParam String responsible_name,
-			@RequestParam String responsible_surname, @RequestParam String address, @RequestParam String telephone,
-			@RequestParam String postal, String image, String password, Map<String, Object> model) {
+	public String addOng(@RequestParam String name, @RequestParam String email, @RequestParam String responsible_name, @RequestParam String responsible_surname, @RequestParam String address, @RequestParam String telephone, @RequestParam String postal, String image, String password, @RequestParam String description, Map<String, Object> model) {
 		model.put("title", "Registrar ONG");
 
 		String enc_password = new BCryptPasswordEncoder().encode(password); // ENCRYPT PASSWORD
 
-		ONG ong = new ONG(name, email, responsible_name, responsible_surname, address, telephone, postal, image,
-				enc_password); // CREATE USER
+		ONG ong = new ONG(name, email, responsible_name, responsible_surname, address, telephone, postal, image, enc_password, description);
 
 		this.ongService.save(ong); // INSERT INTO DATABASE
 
 		return "redirect:index"; // REDIRECTS TO INDEX
 
-	}
-
-	@GetMapping("/ongs") // ONG LIST
-	public Collection<ONG> listOngs() {
-
-		return ongService.getAll();
 	}
 
 }
--- backend/src/main/java/com/group7/voluntaweb/Repositories/ONGRepository.java
+++ backend/src/main/java/com/group7/voluntaweb/Repositories/ONGRepository.java
@@ -1,5 +1,7 @@
 package com.group7.voluntaweb.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.group7.voluntaweb.Models.ONG;
