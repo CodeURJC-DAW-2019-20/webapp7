aea5c7a3fa9e0475924947aae00e219b92f252f1
PabloAtahoneroGB <p.atahonero.2017@alumnos.urjc.es>
2020-03-08 18:03:12 +0100

    CommentRestController Developed

---
+++ backend/src/main/java/com/group7/voluntaweb/services/CommentService.java
@@ -0,0 +1,37 @@
+package com.group7.voluntaweb.services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.group7.voluntaweb.models.Comment;
+import com.group7.voluntaweb.repositories.CommentRepository;
+
+@Service
+public class CommentService {
+
+	@Autowired
+	private CommentRepository comRepo;
+
+	public Collection<Comment> getAllComments() {
+		return comRepo.findAll();
+	}
+
+	public Comment findComment(Long comment_id) {
+
+		return comRepo.findSpecificComment(comment_id);
+	}
+
+	public void saveCommentary(Comment comentary) {
+
+		comRepo.save(comentary);
+
+	}
+
+	public void deleteComment(Long id) {
+		comRepo.deleteById(id);
+
+	}
+
+}
