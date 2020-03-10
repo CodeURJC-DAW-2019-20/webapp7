e7e8c13136bed30f676d3880cb52149b709ea59d
Daniel Serrano Cobos <d.serranoc.2017@alumnos.urjc.es>
2020-03-10 02:45:47 +0100

    fixed docker

--- backend/create-image.sh
+++ backend/create-image.sh
@@ -6,10 +6,6 @@ path_project=$(pwd)
 path_jar=$path_project/target
 
 
-echo "Compiling app"
-cd $path_project
-./mvnw package && java -jar target/gs-spring-boot-docker-0.1.0.jar
-
 cd $path_docker
 mv $path_jar/backend-0.0.1-SNAPSHOT.jar .
 rm backend.jar
