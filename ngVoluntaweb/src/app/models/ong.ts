a99a18b9c3e4deafd7531ace2844740346a55955
dfuente2017 <48331210+dfuente2017@users.noreply.github.com>
2020-03-31 16:16:59 +0200

    Add constructor for my lastest commit
    Chema told me to make the same with a constructor, what a mad man.

---
+++ ngVoluntaweb/src/app/models/ong.ts
@@ -0,0 +1,15 @@
+export class ONG{
+    constructor(
+        public id:number,
+        public nombre:string,
+        public responsibleName:string,
+        public responsibleSurname:string,
+        public description:string,
+        public address:string,
+        public email:string,
+        public postalCode:string,
+        public image:string
+    ){
+
+    }
+}
