d908428307f42bb16cabaf4314c96f0c8b82a593
Daniel Serrano Cobos <d.serranoc.2017@alumnos.urjc.es>
2020-03-31 16:43:27 +0200

    Relations fixed

--- ngVoluntaweb/src/app/models/user.ts
+++ ngVoluntaweb/src/app/models/user.ts
@@ -1,12 +1,18 @@
+import { UserVolunteering } from './uservolunteering';
+import { Like } from './like';
+
 export class User{
     constructor(
         public id: number,
+        public registrations: Set<UserVolunteering>,
+        public likes: Set<Like>,
         public name: string,
         public surname: string,
         public email: string,
         public password: string,
         public city: string,
         public telephone: string,
+        public roles:Array<string>,
         public image: string,
         public registeredAt: Date
     ){
