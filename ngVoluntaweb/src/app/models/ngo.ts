41cb43619355775494b12e8589bba852ab5ad468
Daniel Serrano Cobos <d.serranoc.2017@alumnos.urjc.es>
2020-03-31 17:16:26 +0200

    Fixed properties names

--- ngVoluntaweb/src/app/models/ngo.ts
+++ ngVoluntaweb/src/app/models/ngo.ts
@@ -1,14 +1,19 @@
+import { Volunteering } from './volunteering';
+
 export class NGO{
     constructor(
         public id:number,
         public name:string,
-        public responsibleName:string,
-        public responsibleSurname:string,
+        public responsiblename:string,
+        public responsiblesurname:string,
         public description:string,
         public address:string,
         public email:string,
-        public postalCode:string,
-        public image:string
+        public postal:string,
+        public image:string,
+        public volunteerings: Set<Volunteering>,
+        public telephone: string,
+        public password: string
     ){
 
     }
--- ngVoluntaweb/src/app/models/volunteering.ts
+++ ngVoluntaweb/src/app/models/volunteering.ts
@@ -1,13 +1,17 @@
 import { Category } from './category';
 import { NGO } from './ngo';
+import { UserVolunteering } from './uservolunteering';
+import { Like } from './like';
 
 export class Volunteering{
     constructor(
         public id:number,
+        public joined_user: Set<UserVolunteering>,
+        public likes: Set<Like>,
         public name:string,
         public category:Category,
-        public startDate:Date,
-        public endDate:Date,
+        public startdate:Date,
+        public enddate:Date,
         public description:string,
         public image:string,
         public city:string,
