541c96e95649840685779554d9f23aee86a4c6e0
Daniel Serrano Cobos <d.serranoc.2017@alumnos.urjc.es>
2020-04-02 16:08:50 +0200

    Added assets, created header and footer, installed ng-bootstrap.

--- ngVoluntaweb/src/app/app.module.ts
+++ ngVoluntaweb/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
