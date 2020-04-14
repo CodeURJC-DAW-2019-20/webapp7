e2c71422a12894ff2bb3eab1b7f33c8b5f8bb541
Daniel Serrano Cobos <d.serranoc.2017@alumnos.urjc.es>
2020-04-14 16:32:32 +0200

    TESTING script create-image

--- docker/create-image.sh
+++ docker/create-image.sh
@@ -1,4 +1,25 @@
-cd ../backend
+if ! which mvn > /dev/null; then
+   echo -e "Maven not found. It will install shortly\c"
+   sudo apt-get install maven
+fi
+mkdir ../backend/src/main/resources/static/new/
+cd ../ngVoluntaweb
+
+if ! which node > /dev/null; then
+   echo -e "NodeJS not found. It will install shortly\c"
+   sudo apt install nodejs
+   sudo apt install npm
+fi
+
+sudo npm install --silent
+sudo npm run build
+
+cd dist/ngVoluntaweb
+cp . ../../../backend/src/main/resources/static/new/.
+
+
+
+cd ../../../backend
 mvn clean install
 cd target
 mv backend_src-0.0.1-SNAPSHOT.jar ../../docker/app/backend.jar
