3d3a2bd70ba44481ea1dbcd2997c366762fee707
Daniel Serrano Cobos <d.serranoc.2017@alumnos.urjc.es>
2020-04-16 19:22:13 +0200

    fix error page mvc

--- backend/src/main/java/com/group7/voluntaweb/controllers/CErrorController.java
+++ backend/src/main/java/com/group7/voluntaweb/controllers/CErrorController.java
@@ -4,31 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.group7.voluntaweb.components.UserComponent;
-import com.group7.voluntaweb.models.Comment;
+import com.group7.voluntaweb.components.GenericComponent;
+import com.group7.voluntaweb.helpers.Helpers;
+import com.group7.voluntaweb.models.ONG;
 import com.group7.voluntaweb.models.User;
-import com.group7.voluntaweb.repositories.CommentRepository;
 
 @Controller
 public class CErrorController implements ErrorController {
 	
 	@Autowired
-	private UserComponent userComponent;
+	private GenericComponent genCompo;
 	
 	
     @RequestMapping("/error")
     public String handleError(Model model) {
     	
-    	User user = userComponent.getLoggedUser();
-		boolean logged = userComponent.isLoggedUser();
-		
-		model.addAttribute("user", user);
-		model.addAttribute("logged", logged);
+    	Helpers helper = new Helpers();
+		if (genCompo.getLoggedUser() instanceof User) {
+			User user = (User) genCompo.getLoggedUser();
+			Boolean isAdmin = user.getRoles().contains("ROLE_ADMIN");
+			helper.setNavbar(model, user, null, isAdmin);
+			
+		} else if (genCompo.getLoggedUser() instanceof ONG){
+			ONG ong = (ONG) genCompo.getLoggedUser();
+			helper.setNavbar(model, null, ong, false);
+		}
         model.addAttribute("title", "Error");
         return "error";
     }
